cffdfd4ea66e6e22f92ce7a827e505dce4944c93
CalliEtc <callipter@gmail.com>
2024-03-25 13:09:55 +0100

    avancement de la feature favorite, pas complet mais fonctionnel

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,19 @@
-import { useState } from 'react'
-import './App.css'
+import React, { useState } from 'react';
+import Favorites from './composants/favorite/Favorites';
 
 function App() {
 
+  const data = [
+    { id: 1, name: "Marco" },
+    { id: 2, name: "Lincoln" },
+    { id: 3, name: "Aya" }
+  ];
+  
   return (
-    <>
-    </>
-  )
+    <div className="App">
+      <Favorites data={data} />
+    </div>
+  );
 }
 
-export default App
+export default App;
