321f26ea51b98af53391362c1d03cb0111bb9490
yuuta2s <asmaa.guebba19@gmail.com>
2024-03-28 13:35:19 +0100

    categorie fonctionnelle

--- src/App.jsx
+++ src/App.jsx
@@ -4,23 +4,26 @@ import FavoritesView from './composants/overlay/Favorite/FavoritesContainer';
 import SortOverlay from './composants/overlay/Sort/SortOverlay';
 import { useState } from 'react';
 import Search from './composants/overlay/Search/Search';
-import Categories from './composants/CategorieBar/Categories';
+import Categories from './composants/CategorieBar/Categories.jsx';
 import Ocarina from './composants/Ocarina/Ocarina';
 import Hyrulemap from './composants/HyruleMap/Hyrulemap';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Monsters from './composants/Monsters/Monsters';
 import Treasure from './composants/Treasure/Treasure';
 import Creatures from './composants/Creatures/Creatures';
+import Acceuil from './composants/Acceuil/Acceuil.jsx';
 
 function App() {
   const [manageFavorite, setManageFavorite] = useState(false);
   const [manageSort, setManageSort] = useState(false);
   const [searchView, setSearchView] = useState(false);
+  const [ManageCroissant, setManageCroissant] = useState([]);
+  const [ManageCroissantNum, setManageCroissantNum] = useState([]);
 
   return (
     <Router>
       <div>
-        <header>
+        
           <AppHeader
             setManageFavorite={setManageFavorite}
             manageFavorite={manageFavorite}
@@ -29,21 +32,21 @@ function App() {
             searchView={searchView}
             setSearchView={setSearchView}
           />
+          <Categories/>
           <Search />
-        </header>
         <main>
           <FavoritesView
             setManageFavorite={setManageFavorite}
             manageFavorite={manageFavorite}
           />
-          <SortOverlay setManageSort={setManageSort} manageSort={manageSort} />
+          <SortOverlay setManageSort={setManageSort} manageSort={manageSort} sortByCroissantNum={setManageCroissantNum} setManageCroissant={setManageCroissant} /> 
           <Routes>
             <Route path="/" element={<Categories />} />
             <Route path="/monsters" element={<Monsters />} />
             <Route path="/treasure" element={<Treasure />} />
             <Route path="/creatures" element={<Creatures />} />
             <Route path="/ocarina" element={<Ocarina />} />
-            <Route path="/hyruleMap" element={<Hyrulemap />} />
+            <Route path="/hyrulemap" element={<Hyrulemap />} />
           </Routes>
         </main>
       </div>
--- src/composants/CategorieBar/Categories.jsx
+++ src/composants/CategorieBar/Categories.jsx
@@ -1,90 +1,86 @@
 import { Box } from '@mui/material';
 import { useState } from 'react';
 import '../CategorieBar/categorie.css';
-import { BrowserRouter as Router, Switch, Route, Link, useHistory } from "react-router-dom";
-import Creatures from 'src/composants/Creatures/Creatures.jsx';
-import Monsters from 'src/composants/Monsters/Monsters.jsx';
-import Materials from 'src/composants/Materials/Materials.jsx';
-import Equipment from 'src/composants/Equipment/Equipment.jsx';
-import Treasure from 'src/composants/Treasure/Treasure.jsx';
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Creatures from '../Creatures/Creatures';
+import Monsters from '../Monsters/Monsters'; 
+// import Materials from '../Materials/Materials';
+// import Equipment from '../Equipment/Equipment';
+import Treasure from '../Treasure/Treasure';
 
 function Categories() {
   const [currentNavIcon, setCurrentNavIcon] = useState(null);
   const [pageView, setPageView] = useState(null);
-  const history = useHistory(); // Import du hook useHistory
 
   const handleNavigationClick = function (event) {
     const clickedIcon = event.target.closest(".nav-icon-wrapper");
     if (!clickedIcon) return;
-
+  
     setCurrentNavIcon(clickedIcon.querySelector("img"));
-
+  
     // Au lieu de mettre à jour directement l'état `pageView`, naviguer vers la route appropriée en utilisant React Router
     const pageViewId = clickedIcon.id;
     history.push(pageViewId); // Utilisation de history.push pour naviguer
-
+  
     const navIconWrappers = document.querySelectorAll(".nav-icon-wrapper");
     navIconWrappers.forEach((wrapper) => {
       wrapper.classList.remove("nav-icon-selected");
     });
-
+  
     clickedIcon.classList.add("nav-icon-selected");
   };
 
   return (
-    <Router>
+  
       <section id="navigation-icons">
         <Box display="flex">
-          <Link to="/creatures">
+          
             <Box className={`nav-icon-wrapper ${currentNavIcon?.id === 'creatures' ? 'nav-icon-selected' : ''}`}>
+              <a href='/Creatures'> 
               <img src="src/assets/icons/Creatures.png" alt="Creatures" className="nav-icon" />
+              </a>
+              
             </Box>
-          </Link>
-
-          <Link to="/monsters">
+         
+        
             <Box className={`nav-icon-wrapper ${currentNavIcon?.id === 'monsters' ? 'nav-icon-selected' : ''}`}>
+              <a href='/Monsters'>
               <img src="src/assets/icons/Monsters.png" alt="Monsters" className="nav-icon" />
+              </a>
             </Box>
-          </Link>
+    
 
-          <Link to="/materials">
+       
             <Box className={`nav-icon-wrapper ${currentNavIcon?.id === 'materials' ? 'nav-icon-selected' : ''}`}>
+              <a href='/Materials'>
               <img src="src/assets/icons/Materials.png" alt="Materials" className="nav-icon" />
+              </a>
             </Box>
-          </Link>
+        
 
-          <Link to="/equipment">
+       
             <Box className={`nav-icon-wrapper ${currentNavIcon?.id === 'equipment' ? 'nav-icon-selected' : ''}`}>
+              <a href='/Equipment'>
               <img src="src/assets/icons/Equipment.png" alt="Equipment" className="nav-icon" />
+              </a>
             </Box>
-          </Link>
+       
 
-          <Link to="/treasure">
+         
             <Box className={`nav-icon-wrapper ${currentNavIcon?.id === 'treasure' ? 'nav-icon-selected' : ''}`}>
+              <a href='/Treasure'>
               <img src="src/assets/icons/Treasure.png" alt="Treasure" className="nav-icon" />
+              </a>
             </Box>
-          </Link>
         </Box>
+        {pageView === 'creatures' && <Creatures />}
+        {pageView === 'monsters' && <Monsters />}
+        {pageView === 'materials' && <Materials />}
+       {pageView === 'equipment' && <Equipment />} 
+        {pageView === 'treasure' && <Treasure />}
       </section>
 
-      <Switch>
-        <Route path="/creatures">
-          <Creatures />
-        </Route>
-        <Route path="/monsters">
-          <Monsters />
-        </Route>
-        <Route path="/materials">
-          <Materials />
-        </Route>
-        <Route path="/equipment">
-          <Equipment />
-        </Route>
-        <Route path="/treasure">
-          <Treasure />
-        </Route>
-      </Switch>
-    </Router>
+   
   );
 }
 
